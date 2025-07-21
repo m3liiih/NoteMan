ab733e54a71ee94d32df97aad333072253cde292
m3liiih <kivancahmetmelih@gmail.com>
2025-07-22 02:37:29 +0300

    While loop replacement

--- NoteMan.py
+++ NoteMan.py
@@ -1,12 +1,9 @@
 print("Welcome to NoteMan, your personal Note Taking and Task Management Companion :)")
-def action_type():
-    act_type = input("Select a category: | \"N\" for Notes | \"T\" for Tasks |\n-- ")
-    if act_type.upper() == "N":
-        return act_type
-    elif act_type.upper() == "T":
-        return act_type
-    else:
-        print("\nSelection error, please retry with a valid option.")
-        return action_type()
-act_category = action_type()
-print("\n" + act_category)
+
+def get_category():
+    while True:
+        category = input("Select a category: | \"N\" for Notes | \"T\" for Tasks |\n-- ").upper()
+        if category in ["N", "T"]:
+            return category
+        else:
+            print("\nInvalid selection. Please select a valid category.")
